283bec1c2db048529578d960ac891f91bb92f3a1
Qeric <qericdev@gmail.com>
2022-08-24 12:30:01 -0500

    main tag fix
    Fixed: Cannot be more than one main.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <main className='App align-horizontal-vertical'>
+    <div className='App align-horizontal-vertical'>
       <div className='qr align-horizontal-vertical'>
         <img alt="qrcode that redirects to : https://www.frontendmentor.io/" src="./../images/image-qr-code.png"/>
         <h1>Improve your front-end skills by building projects</h1>
         <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
-    </main>  
+    </div>  
   );
 }
 
