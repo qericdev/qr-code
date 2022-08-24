1c2e50499d93c68840cc618f7a35fe6cb963258a
Qeric <qericdev@gmail.com>
2022-08-24 12:27:00 -0500

    Second Improvement
    Fixing alt property from img.
    Include main tags to HTML.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App align-horizontal-vertical'>
+    <main className='App align-horizontal-vertical'>
       <div className='qr align-horizontal-vertical'>
-        <img src="./../images/image-qr-code.png"/>
+        <img alt="qrcode that redirects to : https://www.frontendmentor.io/" src="./../images/image-qr-code.png"/>
         <h1>Improve your front-end skills by building projects</h1>
         <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
-    </div>  
+    </main>  
   );
 }
 
